f73029511fccdf9e0196b792afe0de70cc14224c
TAKEUCHI Hitoshi <hitoshi@namaraii.com>
2024-09-15 10:53:43 +0900

    面データの追加

---
+++ gd_sokoban/tools/sokoban_converter.rb
@@ -0,0 +1,65 @@
+require 'json'
+require 'fileutils'
+
+def parse_and_save_levels
+  current_level = []
+  level_number = 1
+
+  ARGF.each_line do |line|
+    line = line.rstrip
+
+    next if line.empty? || line.start_with?(";") 
+
+    if line.start_with?("Title:")
+      process_level(level_number, current_level)
+      current_level = []
+      level_number += 1
+    elsif line.match(/^.+\:/) 
+      next
+    else
+      current_level << line.chars
+    end
+  end
+
+  if !current_level.empty?
+    process_level(level_number, current_level)
+  end
+end
+
+def process_level(number, level_data)
+  max_length = level_data.map(&:length).max
+
+  level_data.each do |row|
+    row.fill(' ', row.length...max_length)
+  end
+
+  save_level_as_json(number, level_data)
+end
+
+def save_level_as_json(number, level_data)
+  file_name = format("%03d.json", number)
+
+  converted_level = level_data.map do |row|
+    row.map do |char|
+      case char
+      when '#' then 'wall'
+      when ' ' then 'floor'
+      when '.' then 'goal'
+      when '@' then 'player'
+      when '$' then 'box'
+      else nil  # 不明なキャラクターは無視
+      end
+    end.compact  # nil（無効なキャラクター）は削除
+  end
+
+  File.open(file_name, 'w') do |f|
+    json_data = {
+      level: converted_level
+    }
+    f.write(JSON.pretty_generate(json_data))
+  end
+end
+
+if __FILE__ == $0
+  parse_and_save_levels
+end
